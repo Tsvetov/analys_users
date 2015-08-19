3d900b9a84775b1bbd72efc0c56aac7ce7de0def
cpn <cpn@cpn>
2015-08-19 16:50:59 +0400

    Добавление валидатора на неотрицательность значений.

--- forms.py
+++ forms.py
@@ -1,15 +1,18 @@
 # -*-coding: utf-8 -*-
 
-__author__ = 'cpn'
-
 # django
 from django import forms
+from django.core.exceptions import ValidationError
+
+
+def validate_non_negative(value):
+    """
+        Валидатор для проверки идентификатора на неотрицательность
+    """
+    if value < 0:
+        raise ValidationError(u'%s is not an non-negative number' % value)
 
 
 class UsersForm(forms.Form):
-    id_user_1 = forms.IntegerField()
-    id_user_2 = forms.IntegerField()
-
-
-
-
+    id_user_1 = forms.IntegerField(validators=[validate_non_negative])
+    id_user_2 = forms.IntegerField(validators=[validate_non_negative])
